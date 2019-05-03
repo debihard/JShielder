79f64abb9c728238088f0b1677af5fbc4a5eb729
John <50121345+debihard@users.noreply.github.com>
2019-05-03 20:47:05 +0300

    Update jshielder.sh

--- UbuntuServer_18.04LTS/jshielder.sh
+++ UbuntuServer_18.04LTS/jshielder.sh
@@ -258,13 +258,14 @@ rsa_keycopy(){
     echo " ssh-copy-id -i $HOME/.ssh/id_rsa.pub $username@$serverip "
     say_done
 }
+
 ##############################################################################################################
 # Add manually the Generated Public Key
 rsa_add(){
 echo -n " Do you want add your user public key mannually? (y/n): "; read rsa_add_answer
 if [ "$rsa_add_answer" == "y" ]; then
 echo -n " Enter your public key here and press enter: ";read publickey
-cat $publickey >> /home/$username/.ssh/authorized_keys
+echo "$publickey" >> /home/$username/.ssh/authorized_keys
  echo ""
   echo "Your key is successfully add!"
       say_done   
