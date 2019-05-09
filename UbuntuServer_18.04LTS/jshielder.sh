09923e08d67965bfe868dc7633ecf4ee3d7f3439
John <50121345+debihard@users.noreply.github.com>
2019-05-09 15:21:28 +0500

    Update jshielder.sh

--- UbuntuServer_18.04LTS/jshielder.sh
+++ UbuntuServer_18.04LTS/jshielder.sh
@@ -1762,7 +1762,7 @@ echo "26. Enable Unnatended Upgrades"
 echo "27. Enable Process Accounting"
 echo "28. Install PHP Suhosin (Disabled for Now)"
 echo "29. Install and Secure MySQL"
-echo "30. Create MySQL user and database
+echo "30. Create MySQL user and database"
 echo "31. Set More Restrictive UMASK Value (027)"
 echo "32. Secure /tmp Directory"
 echo "33. Install PSAD IDS"
