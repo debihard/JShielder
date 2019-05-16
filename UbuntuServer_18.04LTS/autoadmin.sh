2d6fd4fdcf8769a5a01fffe947adc0b86cd0b1ee
John <50121345+debihard@users.noreply.github.com>
2019-05-17 02:30:54 +0500

    Update autoadmin.sh

--- UbuntuServer_18.04LTS/autoadmin.sh
+++ UbuntuServer_18.04LTS/autoadmin.sh
@@ -1258,7 +1258,7 @@ file_permissions(){
   df --local -P | awk {'if (NR!=1) print $6'} | xargs -I '{}' find '{}' -xdev -type d -perm -0002 2>/dev/null | xargs chmod a+t
 
   echo " OK"
-  say_done
+  say_done_2
 
 }
 ##############################################################################################################
