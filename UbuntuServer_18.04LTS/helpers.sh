64a69230bd9cd5f31f0d68ccd9b98a0482f3cb61
John <50121345+debihard@users.noreply.github.com>
2019-05-20 19:20:36 +0500

    Update helpers.sh

--- UbuntuServer_18.04LTS/helpers.sh
+++ UbuntuServer_18.04LTS/helpers.sh
@@ -71,4 +71,4 @@ IP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,
 
 NEW_MYSQL_PASSWORD=mysql_root_passw_"$(pwgen 22 1)"_"$IP"
 
-NEW_SERVER_ROOT_PASSWORD=new_root_passw_'$(pwgen 22 1)'_'$IP'
+NEW_SERVER_ROOT_PASSWORD=new_root_passw_"$(pwgen 22 1)"_"$IP"
