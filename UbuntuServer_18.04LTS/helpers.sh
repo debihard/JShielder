672bd98ac40b6a12993b27cd06fb5cf42eb9e65c
John <50121345+debihard@users.noreply.github.com>
2019-05-20 19:49:25 +0500

    Update helpers.sh

--- UbuntuServer_18.04LTS/helpers.sh
+++ UbuntuServer_18.04LTS/helpers.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+apt install -y pwgen
+
+## Autodetect IP address and pre-fill for the user
+IP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
+
+NEW_MYSQL_PASSWORD=mysql_root_passw_"$(pwgen 22 1)"_"$IP"
+
+NEW_SERVER_ROOT_PASSWORD=new_root_passw_"$(pwgen 22 1)"_"$IP"
+
 
 spinner ()
 {
@@ -64,11 +73,3 @@ function tunning() {
     cp templates/$whoapp /home/$username/.$whoapp
     chown $username:$username /home/$username/.$whoapp
 }
-
-
-## Autodetect IP address and pre-fill for the user
-IP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
-
-NEW_MYSQL_PASSWORD=mysql_root_passw_"$(pwgen 22 1)"_"$IP"
-
-NEW_SERVER_ROOT_PASSWORD=new_root_passw_"$(pwgen 22 1)"_"$IP"
